5d6265a6eecb6b6e0c91fd5cb4837ac7619022c2
Ramaldo Martin <rmartin@osisoft.com>
2022-07-14 05:43:20 -0700

    fix: properly implement initialisation of state vector

--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -34,21 +34,27 @@ class Track:
         # unassigned measurement transformed from sensor to vehicle coordinates
         # - initialize track state and track score with appropriate values
         ############
+        pos_sens = np.ones((4, 1)) # homogeneous coordinates
+        pos_sens[0:3] = meas.z[0:3]
+        pos_veh = meas.sensor.sens_to_veh*pos_sens
+       
+        self.x = np.asmatrix(np.zeros((6,1)))
+        self.x[0:3] = pos_veh[0:3]
+  
+        sigma_vx = params.sigma_p44     # initial setting for estimation error covariance P entry for vx
+        sigma_vy = params.sigma_p55     # initial setting for estimation error covariance P entry for vy
+        sigma_vz = params.sigma_p66     # initial setting for estimation error covariance P entry for vz
 
-        self.x = np.matrix([[49.53980697],
-                        [ 3.41006279],
-                        [ 0.91790581],
-                        [ 0.        ],
-                        [ 0.        ],
-                        [ 0.        ]])
-        self.P = np.matrix([[9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 6.4e-03, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+01]])
-        self.state = 'confirmed'
-        self.score = 0
+        P_pos = M_rot * meas.R * M_rot.transpose()
+        P_vel = np.matrix([[sigma_vx**2, 0, 0], # velocity estimation error covariance matrix
+                            [0, sigma_vy**2, 0], 
+                            [0, 0, sigma_vz**2]])
+        self.P = np.matrix(np.zeros((6, 6)))
+        self.P[0:3, 0:3] = P_pos
+        self.P[3:6, 3:6] = P_vel
+
+        self.state = 'initialized'
+        self.score = 1./params.window
         
         ############
         # END student code
@@ -107,9 +113,14 @@ class Trackmanagement:
             if meas_list: # if not empty
                 if meas_list[0].sensor.in_fov(track.x):
                     # your code goes here
-                    pass 
+                    track.score -= 1./params.window
 
         # delete old tracks   
+        for track in self.track_list:
+            if ((track.P[0, 0] > params.max_P or track.P[1, 1] > params.max_P) or
+                ((track.state == 'initialized' or track.state == 'tentative') and track.score < 1./params.window) or 
+                (track.state == 'confirmed' and track.score < params.delete_threshold)):
+                self.delete_track(track)
 
         ############
         # END student code
@@ -136,12 +147,17 @@ class Trackmanagement:
     def handle_updated_track(self, track):      
         ############
         # TODO Step 2: implement track management for updated tracks:
-        # - increase track score
-        # - set track state to 'tentative' or 'confirmed'
-        ############
-
-        pass
+        tentative_threshold = 0.4
         
+        # - increase track score
+        track.score += 1./params.window
+
+        # - set track state to 'tentative' or 'confirmed'
+        if track.state == 'tentative' and track.score > params.confirmed_threshold:
+            track.state = 'confirmed'
+        elif track.state == 'initialized' and track.score > tentative_threshold:
+            track.state = 'tentative'
+                 
         ############
         # END student code
         ############ 
